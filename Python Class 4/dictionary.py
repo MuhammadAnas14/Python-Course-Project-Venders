6f6ef2f332ca261033ef8696342bfb71b5835b3b
Anas452 <anasm9877@gmail.com>
2019-09-03 00:06:30 +0500

    Class 4 codes

---
+++ Python Class 4/dictionary.py
@@ -0,0 +1,24 @@
+
+student = {name:"anas", "age":20, "class":6}
+
+name_of_student = student["name"]
+print(name_of_student)
+
+student["School"] = "ABC school"
+print(student)
+
+student["age"] = 30
+print(student)
+
+del(student["name"])
+print(student)
+
+student.clear()
+print(student)
+
+del(student)
+print(student)
+
+
+# github.com
+# Download the Git 
