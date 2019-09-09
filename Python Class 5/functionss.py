c986717c1b9c185a5289ed663a6ff451544fbfba
Anas452 <anasm9877@gmail.com>
2019-09-09 16:47:16 +0500

    class5 code

---
+++ Python Class 5/functionss.py
@@ -0,0 +1,34 @@
+def new(x):
+    print(x)
+
+new(3)
+
+def take(x,y):
+    z=x+y
+    return z
+
+a = take(3,6)
+print(a)
+
+x = "hello"
+
+print(x[2])
+
+def hii(x):
+    x= "world"
+    print(x)
+
+print(x)
+hii(x)
+print(x)
+
+
+my_list=[1,2,3,4]
+
+def change(x):
+    my_list[2]=7
+    print(my_list)
+
+print(my_list)
+change(my_list)
+print(my_list)
---
+++ Python Class 5/looping_through_list.py
@@ -0,0 +1,23 @@
+matrice = [[1,2],[4,8]]
+
+print(matrice[1][0])
+
+
+
+
+my_list = [2,5,6,7,8,10]
+
+for element in my_list:
+    print(element+1)
+    print(element*2)
+
+matrice = [[1,2],[4,8]]
+
+for i in range(0,2):
+    for j in range(0,2):
+        print(matrice[i][j])
+
+
+
+
+
